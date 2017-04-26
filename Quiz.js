9c7907f46b3309594b060e72fc73fd4d995087eb
g00329059 <g00329059@gmit.ie>
2017-04-26 16:07:29 +0100

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,19 @@
+function check(){
+
+document.getElementById("picture").style.visibility = "visible";
+}
+
+function check1(){
+
+document.getElementById("picture1").style.visibility = "visible";
+}
+
+function check2(){
+
+document.getElementById("picture2").style.visibility = "visible";
+}
+
+function check3(){
+
+document.getElementById("picture3").style.visibility = "visible";
+}
